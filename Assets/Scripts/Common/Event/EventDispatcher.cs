432153eeac238652efdc96927958386e5ddff494
NaturaAurum <natura0321@gmail.com>
2019-05-16 00:04:28 +0900

    자동으로 namespace달아서 스크립트 생성해주는 EditorScript 추가 기본적인 구조들 잡는 중.

---
+++ Assets/Scripts/Common/Event/EventDispatcher.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AM.Common.Event
+{
+    /// <summary>
+    /// Event들이 상속받는 인터페이스
+    /// </summary>
+    public interface IEvent {}
+    
+    /// <summary>
+    /// Event 관리자
+    /// </summary>
+    public class EventDispatcher
+    {
+        /// <summary>
+        /// EventHandler Delegate
+        /// </summary>
+        /// <param name="evt">뿌려질 Event Type</param>
+        /// <typeparam name="T">Event Type</typeparam>
+        public delegate void EventHandler<T>(T evt) where T : IEvent;
+        
+        private static Dictionary<Type, List<Delegate>> handlerDict = new Dictionary<Type, List<Delegate>>();
+
+        /// <summary>
+        /// Handler Dictionary중에서 Type에 맞는 HandlerList를 뽑아온다
+        /// Type이 Dictionary에 없다면 추가한다.
+        /// </summary>
+        /// <typeparam name="T">Event Type</typeparam>
+        /// <returns>Handler List</returns>
+        private static List<Delegate> GetHandlerList<T>() where T : IEvent
+        {
+            var type = typeof(T);
+            
+            handlerDict.TryGetValue(type, out List<Delegate> handlerList);
+
+            if (handlerList == null)
+            {
+                handlerDict.Add(type, new List<Delegate>());
+            }
+
+            return handlerList;
+        }
+
+        /// <summary>
+        /// 리스너 추가
+        /// </summary>
+        /// <param name="handler">handler function</param>
+        /// <typeparam name="T">Event Type</typeparam>
+        public static void Listen<T>(EventHandler<T> handler) where T : IEvent
+        {
+            var handlerList = GetHandlerList<T>();
+            handlerList.Add(handler);
+        }
+
+        public static void Remove<T>(EventHandler<T> handler) where T : IEvent
+        {
+            var handlerList = GetHandlerList<T>();
+            handlerList.Remove(handler);
+        }
+
+        public static void Send<T>(T evt) where T : IEvent
+        {
+            handlerDict.TryGetValue(typeof(T), out List<Delegate> handlerList);
+
+            Debug.Assert(handlerList != null, $"There is no listener of {nameof(T)}");
+
+            foreach (var handler in handlerList)
+            {
+                (handler as EventHandler<T>)?.Invoke(evt);
+            }
+        }
+    }
+}
---
+++ Assets/SwingBy/Scripts/Core/Interfaces/IGravityObject.cs
@@ -0,0 +1,10 @@
+﻿namespace AM.SwingBy.Core.Interfaces
+{
+    /// <summary>
+    /// 중력 영향을 받는 오브젝트들이 상속받는 인터페이스
+    /// </summary>
+    public interface IGravityObject
+    {
+        
+    }
+}
